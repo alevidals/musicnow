77a2d917674ebff379285044e7e659486a5c8255
Alejandro Vidal <alejandro.vidal@iesdonana.org>
2020-03-17 12:26:47 +0100

    Añado la base de datos

--- db/musicnow.sql
+++ db/musicnow.sql
@@ -2,3 +2,63 @@
 -- Archivo de base de datos --
 ------------------------------
 
+DROP TABLE IF EXISTS usuarios CASCADE;
+
+CREATE TABLE usuarios
+(
+    id            BIGSERIAL    PRIMARY KEY
+  , login         VARCHAR(50)  NOT NULL UNIQUE
+  , nombre        VARCHAR(255) NOT NULL
+  , apellidos     VARCHAR(255) NOT NULL
+  , email         VARCHAR(255) NOT NULL UNIQUE
+  , password      VARCHAR(255) NOT NULL
+  , fnac          DATE
+  , rol           VARCHAR(255) NOT NULL
+  , auth_key      VARCHAR(255)
+  , confirm_token VARCHAR(255)
+  , created_at    TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS generos CASCADE;
+
+CREATE TABLE generos
+(
+    id           BIGSERIAL    PRIMARY KEY
+  , denominacion VARCHAR(255) NOT NULL UNIQUE
+  , created_at   TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS albumes CASCADE;
+
+CREATE TABLE albumes
+(
+    id         BIGSERIAL     PRIMARY KEY
+  , titulo     VARCHAR(255)  NOT NULL
+  , anyo       NUMERIC(4)    NOT NULL
+  , created_at TIMESTAMP(0)  NOT NULL DEFAULT CURRENT_TIMESTAMP
+  , usuario_id BIGINT        NOT NULL REFERENCES usuarios (id)
+);
+
+DROP TABLE IF EXISTS canciones CASCADE;
+
+CREATE TABLE canciones
+(
+    id          BIGSERIAL     PRIMARY KEY
+  , titulo      VARCHAR(255)  NOT NULL
+  , album_id    BIGINT        NOT NULL REFERENCES albumes (id)
+  , genero_id   BIGINT        NOT NULL REFERENCES generos (id)
+  , url_cancion VARCHAR(2048) NOT NULL
+  , url_portada VARCHAR(2048) NOT NULL
+  , anyo        NUMERIC(4)    NOT NULL
+  , duracion    INTERVAL      NOT NULL
+  , created_at  TIMESTAMP(0)  NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS albumes_canciones CASCADE;
+
+CREATE TABLE albumes_canciones
+(
+    id           BIGSERIAL PRIMARY KEY
+  , album_id     BIGINT    NOT NULL REFERENCES albumes (id)
+  , canciones_id BIGINT    NOT NULL REFERENCES canciones (id)
+);
