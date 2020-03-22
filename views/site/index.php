4982b41b1b555ff095dedfb301b9d358d6101a09
Alejandro Vidal <alejandro.vidal@iesdonana.org>
2020-03-22 01:06:55 +0100

    [MOD] Cambio el diseño de generos/index

--- views/site/index.php
+++ views/site/index.php
@@ -30,13 +30,28 @@ $this->registerJS($js);
         <div> <?= Html::img('@web/img/banner5.png'); ?> </div>
     </div>
 
-    <div class="row mt-5">
-        <?php foreach ($canciones as $cancion) : ?>
-            <div class="col-12 col-md-6 col-lg-4">
-                <?= Html::img($cancion->url_portada, ['class' => 'img-fluid my-2']) ?>
-                <p class="text-white mb-0"><?= $cancion->titulo?></p>
-                <p class="text-muted mt-0"><?= $cancion->getAlbum()->one()->titulo?></p>
+    <div class="row">
+        <div class="col-6 mt-5">
+            <?php foreach ($canciones as $cancion) : ?>
+                <div class="row">
+                    <div class="col">
+                        <?= Html::img($cancion->url_portada, ['class' => 'img-fluid']) ?>
+                        <p class="text-white mb-0"><?= $cancion->titulo?></p>
+                        <p class="text-muted mt-0"><?= $cancion->getAlbum()->one()->titulo?></p>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+        <div class="col-4 mt-5 ml-auto">
+            <div class="row">
+                <div class="col-3">
+                    <?= Html::img('https://pixabay.com/get/57e7d7444e5aaa14f1dc8460825668204022dfe05b55794a712c79d1/delete-1727486_640.png', ['class' => 'img-fluid']) ?>
+                </div>
+                <div class="col">
+                    <p class="mt-0 p-0"><?= Yii::$app->user->identity->login ?></p>
+                    <p class="p-0 m-0"><?= Yii::$app->user->identity->nombre . ' ' .  Yii::$app->user->identity->apellidos ?></p>
+                </div>
             </div>
-        <?php endforeach; ?>
+        </div>
     </div>
 </div>
